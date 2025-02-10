cc72f8dad15fa7ce15158331f381ac9dc761a4ff
Serg-ux <github.stingray458@passmail.net>
2025-02-10 12:15:42 +0100

    parsear argumentos

--- F1nd3r.py
+++ F1nd3r.py
@@ -6,3 +6,8 @@ import argparse
 parser = argparse.ArgumentParser(description='Consulta crt.sh para un dominio y procesa los resultados.')
 parser.add_argument('domain', type=str, help='El dominio que deseas consultar (por ejemplo, google.com)')
 
+# Parsear los argumentos
+args = parser.parse_args()
+
+# Obtener el dominio desde los argumentos
+domain = args.domain
