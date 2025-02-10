ccbd63d8b4c20ec12ac9d84b84b40ab8330346f9
Serg-ux <github.stingray458@passmail.net>
2025-02-10 11:31:26 +0100

    Config parser

--- F1nd3r.py
+++ F1nd3r.py
@@ -1,3 +1,8 @@
 import requests
 import json
 import argparse
+
+# Configurar el parser de argumentos
+parser = argparse.ArgumentParser(description='Consulta crt.sh para un dominio y procesa los resultados.')
+parser.add_argument('domain', type=str, help='El dominio que deseas consultar (por ejemplo, google.com)')
+
